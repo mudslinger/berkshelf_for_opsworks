acb83a6f4feff99875990d16777182cac5e37ff2
hisato tanaka <hisato.tanaka@gmail.com>
2014-08-11 11:45:04 +0900

    31421

--- timezone-jp/recipes/default.rb
+++ timezone-jp/recipes/default.rb
@@ -1,5 +1,4 @@
-# sudo "mv /etc/localtime /etc/localtime.#{Time.now.to_i}"	
-# sudo "ln -s /usr/share/zoneinfo/Asia/Tokyo /etc/localtime"
+
 
 link "/usr/share/zoneinfo/Asia/Tokyo" do
 	to "/etc/localtime"
