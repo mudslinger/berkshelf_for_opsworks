4cb95cec0c91e6b29414eff6c398effd245e856b
hisato tanaka <hisato.tanaka@gmail.com>
2014-08-12 13:12:05 +0900

    spdy rewrite

--- nginx_unicorn_rewrite/recipes/default.rb
+++ nginx_unicorn_rewrite/recipes/default.rb
@@ -1,7 +1,13 @@
-template "app.conf" do
-	path '/home/ec2-user/test.conf'
-	source 'app.conf.erb'
-	owner 'root'
-	group 'root'
-	mode 0644
+# template "app.conf" do
+# 	path '/home/ec2-user/test.conf'
+# 	source 'app.conf.erb'
+# 	owner 'root'
+# 	group 'root'
+# 	mode 0644
+# end
+
+file '/etc/nginx/sites-available/miniapps' do
+	_file = Chef::Util::FileEdit.new(path)
+	_file.search_file_replace('listen   443;', "listen   443 ssl spdy;")
+	content _file.send(:editor).lines.join
 end
